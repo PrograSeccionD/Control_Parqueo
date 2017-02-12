093d0b9c3f547084d62d6c4cc54083f950bc90f5
Oscar Gomez <Oscar Gomez>
2017-02-12 16:34:13 -0600

    'mi_primer_cambio'

--- src/control_paqueo_d/Control_Paqueo_D.java
+++ src/control_paqueo_d/Control_Paqueo_D.java
@@ -15,7 +15,7 @@ public class Control_Paqueo_D {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Hola Gruito de Atras
     }
     
 }
